15e5e862d1266e8633b664d39d72db305d1aa2a9
Gregory Casamento <greg.casamento@gmail.com>
2023-08-30 10:53:59 -0400

    Initial Commit

---
+++ NSConditionLock_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSConditionLock_test
+//
+//  Created by Gregory John Casamento on 8/30/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
